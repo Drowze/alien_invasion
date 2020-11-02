b113cc2d46386446a2ad852efa2bcfba5101ff29
Drowze <gibim6@gmail.com>
2020-11-02 05:03:44 +0000

    Add an alien!

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
   """Overall class to manage game assets and behaviour"""
@@ -20,6 +21,9 @@ class AlienInvasion:
     pygame.display.set_caption('Alien Invasion')
     self.ship = Ship(self)
     self.bullets = pygame.sprite.Group()
+    self.aliens = pygame.sprite.Group()
+
+    self._create_fleet()
 
   def run_game(self):
     """Start the main loop for the game."""
@@ -57,6 +61,12 @@ class AlienInvasion:
     elif event.key == pygame.K_LEFT:
       self.ship.moving_left = False
 
+  def _create_fleet(self):
+    """Create the fleet of aliens"""
+    # Make an alien
+    alien = Alien(self)
+    self.aliens.add(alien)
+
   def _fire_bullet(self):
     """Create a new bullet and add it to the bullets group"""
     if len(self.bullets) < self.settings.bullets_allowed:
@@ -79,6 +89,8 @@ class AlienInvasion:
     self.ship.blitme()
     for bullet in self.bullets.sprites():
       bullet.draw_bullet()
+    
+    self.aliens.draw(self.screen)
 
     pygame.display.flip()
 
